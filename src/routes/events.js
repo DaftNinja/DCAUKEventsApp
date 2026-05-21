a73d1f75297a327b35bb6caab0d2bbf3e9a6ec58
DaftNinja <andrew.mccreath@gmail.com>
2026-05-21 12:05:13 +0100

    Remove duplicate eq import from drizzle-orm

--- src/routes/events.js
+++ src/routes/events.js
@@ -1,4 +1,3 @@
-import { eq } from "drizzle-orm";
 import { users } from "../db/schema.js";
 import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);import { Router } from "express";
